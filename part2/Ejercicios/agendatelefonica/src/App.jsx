d924db075e73a4add046d9f45e372512403a5c5c
dcrespounal <dcrespo@unal.edu.co>
2025-05-07 11:21:21 -0500

    Actualizacion agenda comunicación servidor

--- part2/Ejercicios/agendatelefonica/src/App.jsx
+++ part2/Ejercicios/agendatelefonica/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { PersonForm, Filter, AllPersons } from './components/Agenda'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([]) //El valor inicial de persons es un array vacío
   const [newName, setNewName] = useState('')
   const [newTelephone, setNewTelephone] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons') //Realiza una solicitud GET a la URL especificada
+      .then(response => {
+        console.log('promise fulfilled') //Cuando se cumple la promesa, se ejecuta esta función
+        setPersons(response.data) //Actualiza el estado de notes con los datos recibidos de la respuesta
+      })
+  }, []) //El segundo argumento vacío [] significa que el efecto solo se ejecutará una vez, al montar el componente
+  //El efecto se ejecuta después de que el componente se haya montado y cada vez que se actualice
+  console.log('render', persons.length, 'notes') //Muestra en la consola el número de notas que se han cargado  
 
   const addPerson = (event) => {
     event.preventDefault()
