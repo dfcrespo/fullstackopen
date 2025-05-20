194a9ad4bd38e42d1dcd48df921e4b4ecc4ac14b
dcrespounal <dcrespo@unal.edu.co>
2025-05-20 11:15:46 -0500

    Actualizacion part3

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-
 let notes = [
   {
     id: 1,
@@ -19,6 +18,8 @@ let notes = [
     important: true
   }
 ]
+
+app.use(express.json()) //Middleware para parsear el cuerpo de la petición a JSON
 
 app.get('/', (request, response) => { //Controlador de eventos para la ruta raíz, dos parametros request y response, request es la petición y response es la respuesta
   response.send('<h1>Hello World!</h1>')
@@ -43,6 +44,33 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 const PORT = 3001
